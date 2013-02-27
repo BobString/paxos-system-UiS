e2375a69c51b8a76ce42b91df1e3e3f4cea2fea9
remypannaud <remy.pannaud@gmail.com>
2013-02-27 10:54:20 +0100

    signals type (except Prepare : made of only an int)

---
+++ paxos/signals/signals.go
@@ -0,0 +1,24 @@
+package signals
+
+
+
+type PromiseType struct {
+	Current int
+	LastRound int
+	LastValue string
+}
+
+type AcceptType struct {
+	RoundNum int
+	Value string
+}
+
+type AcceptType struct {
+	RoundNum int
+	Value string
+}
+
+type LearnType struct {
+	RoundNum int
+	Value string
+}
