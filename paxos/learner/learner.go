10cd10fdfb91d62a6d24123dd811d7bc02c237c5
barushka <barushka@gmail.com>
2013-02-27 13:23:27 +0100

    Update paxos/learner/learner.go

--- paxos/learner/learner.go
+++ paxos/learner/learner.go
@@ -1,6 +1,29 @@
 package learner
 
 import (
+  "acceptor"
+	"fmt"
+	"connector"
+)
+
+var(
 
 )
 
+type Pair struct {
+	nv int
+	val string
+}
+
+func receivingMsgs (incLearnerMsgs chan string)
+{
+	res = strings.Split(mesg,"@")
+	
+	x = res[1]
+	y = res[2]
+	p = Pair {res[1], res[2]}
+
+	PairMap [Pair] int
+	
+	_,ok = PairMap [Pair{x, y}]
+}
