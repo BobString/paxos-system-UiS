241d37382b4961a4091f7ddf6f241474eefa6b77
Remy Pannaud <remy.pannaud@gmail.com>
2013-03-08 15:04:07 +0100

    Client improved
    for the tests

--- main/valueClient.go
+++ main/valueClient.go
@@ -3,22 +3,43 @@ package main
 import (
 	"connector"
 	"fmt"
+	"os"
 )
 
 func main() {
-	var val string
+	var val,menu,mess string
 	var pr int
-	fmt.Println("Enter the value to send")
-	_, err := fmt.Scanln(&val)
+	for {
+		fmt.Println("Choose what you want to send")
+		fmt.Println("1 : Value")
+		fmt.Println("2 : StopServer")
+		fmt.Println("3 : RestoreServer")
+		fmt.Println("4 : Exit program")
+		_, err := fmt.Scanln(&menu)
+		checkErr(err)
+		if menu == 4 {
+			os.Exit(0)
+		} 
+		switch menu {
+			case 1:
+				fmt.Println("Enter the value to send")
+				_, err := fmt.Scanln(&val)
+				checkErr(err)	
+				mess = "Value@" + val
+			case 2:
+				mess = "StopServer@"
+			case 3:
+				mess = "RestoreServer@"
+		}
+		fmt.Println("Please select a process between 1 and 3")
+		_, err = fmt.Scanln(&pr)
+		checkErr(err)
+		connector.Send(mess, pr, nil)
+	}
+}
+
+func checkErr (err error) {
 	if err != nil {
 		fmt.Println("read error")
 	}
-	valueMess := "Value@" + val
-	//fmt.Println("Message to be sent :", valueMess)
-	fmt.Println("Please select a process between 1 and 3")
-	_, err = fmt.Scanln(&pr)
-	if err != nil {
-		fmt.Println("read error")
-	}
-	connector.Send(valueMess, pr, nil)
 }
